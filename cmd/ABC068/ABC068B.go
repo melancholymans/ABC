aa2847803f2afe8b5169dd8df97c66e617d421be
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-03-22 06:11:31 +0900

    ABC068B.go modified

--- cmd/ABC068/ABC068B.go
+++ cmd/ABC068/ABC068B.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -14,5 +15,54 @@ func main() {
 	sc.Scan()
 	s := sc.Text()
 	n, _ := strconv.Atoi(s)
-	fmt.Fprintln(writer, n)
+	max := 0
+	result := n
+	for i := 2; i <= n; i++ {
+		_, m := Divisor(i)
+		if max < m[2] {
+			max = m[2]
+			result = i
+		}
+	}
+	fmt.Fprintln(writer, result)
+}
+
+func Divisor(n int) ([]int, map[int]int) {
+	sqrtn := int(math.Sqrt(float64(n)))
+	c := 2
+	divisor := []int{}
+	divisorm := make(map[int]int)
+	for {
+		if n%2 != 0 {
+			break
+		}
+		divisor = append(divisor, 2)
+		divisorm[2]++
+		n /= 2
+	}
+	c = 3
+	for {
+		if n%c == 0 {
+			divisor = append(divisor, c)
+			divisorm[c]++
+			n /= c
+		} else {
+			c += 2
+			if c > sqrtn {
+				break
+			}
+		}
+	}
+	if n != 1 {
+		divisor = append(divisor, n)
+		divisorm[n]++
+	}
+	return divisor, divisorm
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
