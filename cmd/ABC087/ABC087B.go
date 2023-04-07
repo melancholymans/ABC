6d90b326dd6870de90a7019990043a11bf3b091d
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-08 05:56:34 +0900

    ABC087B.go add

---
+++ cmd/ABC087/ABC087B.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	b, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	c, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	x, _ := strconv.Atoi(sc.Text())
+	count := 0
+	for i := 0; i <= a; i++ {
+		for j := 0; j <= b; j++ {
+			for k := 0; k <= c; k++ {
+				if 500*i+100*j+50*k == x {
+					count++
+				}
+			}
+		}
+	}
+	fmt.Fprintln(writer, count)
+}
