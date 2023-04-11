86b1101d7abae701f62bb038d2fce67cb4dc9db3
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-11 19:59:11 +0900

    ABC031A.go add

---
+++ cmd/ABC031/ABC031A.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	r := strings.Split(sc.Text(), " ")
+	a, _ := strconv.Atoi(r[0])
+	d, _ := strconv.Atoi(r[1])
+	fmt.Fprintln(writer, (Min(a, d)+1)*Max(a, d))
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
