8fb712710820d831d3b6cd85706f3f77d2531601
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-13 09:39:40 +0900

    ABC035A.go modified

--- cmd/ABC035/ABC035A.go
+++ cmd/ABC035/ABC035A.go
@@ -16,5 +16,9 @@ func main() {
 	r := strings.Split(sc.Text(), " ")
 	w, _ := strconv.Atoi(r[0])
 	h, _ := strconv.Atoi(r[1])
-	fmt.Fprintln(writer, w, h)
+	if w%16 == 0 && h%9 == 0 {
+		fmt.Fprintln(writer, "16:9")
+	} else {
+		fmt.Fprintln(writer, "4:3")
+	}
 }
