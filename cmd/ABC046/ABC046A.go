879c153a995f04322118db505791ea4d684228c7
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-18 06:57:34 +0900

    ABC046A.go add

---
+++ cmd/ABC046/ABC046A.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	r := strings.Split(sc.Text(), " ")
+	a, _ := strconv.Atoi(r[0])
+	b, _ := strconv.Atoi(r[1])
+	c, _ := strconv.Atoi(r[2])
+	m := map[int]int{}
+	m[a] += 1
+	m[b] += 1
+	m[c] += 1
+	fmt.Fprintln(writer, len(m))
+}
