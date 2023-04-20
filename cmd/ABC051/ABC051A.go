58f5842ba7c28721a18103fed27a744b0a09fb19
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-21 05:37:03 +0900

    ABC051A.go add

---
+++ cmd/ABC051/ABC051A.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	r := strings.Split(sc.Text(), ",")
+	fmt.Fprintln(writer, strings.Join(r, " "))
+}
