b2ef47b134fc79fd6849d4c5b9399fb6b495a4b5
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-24 05:30:13 +0900

    ABC058A.go add

---
+++ cmd/ABC058/ABC058A.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	r := strings.Split(sc.Text(), " ")
+	a, _ := strconv.Atoi(r[0])
+	b, _ := strconv.Atoi(r[1])
+	c, _ := strconv.Atoi(r[2])
+	if b-a == c-b {
+		fmt.Fprintln(writer, "YES")
+	} else {
+		fmt.Fprintln(writer, "NO")
+	}
+}
