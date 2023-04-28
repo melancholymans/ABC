4d992ac9684b7a16bde1075178301eca24cf68f5
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-29 05:21:00 +0900

    ABC068A.go add

---
+++ cmd/ABC068/ABC068A.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	n := sc.Text()
+	fmt.Fprintln(writer, "ABC"+n)
+}
