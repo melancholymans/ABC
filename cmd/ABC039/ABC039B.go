a44a81e751889e0fe5881e8bfaaa782eab66f842
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-04-29 07:19:28 +0900

    ABC039B.g add

---
+++ cmd/ABC039/ABC039B.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	n, _ := strconv.ParseFloat(sc.Text(), 64)
+	fmt.Fprintln(writer, math.Sqrt(math.Sqrt(n)))
+}
