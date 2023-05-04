b62341e4d1c7dba017330738f0c3adf613d073b5
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-05-05 05:30:36 +0900

    ABC085A.go add

---
+++ cmd/ABC085/ABC085A.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	s := sc.Text()
+	s = strings.Replace(s, "2017", "2018", 1)
+	fmt.Fprintln(writer, s)
+}
