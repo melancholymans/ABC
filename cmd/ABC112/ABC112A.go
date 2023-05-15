6b5356cedd1ea0abaad5718a38801f90609c172d
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-05-16 05:32:55 +0900

    ABC112A.go add

---
+++ cmd/ABC112/ABC112A.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+	if n == 1 {
+		fmt.Fprintln(writer, "Hello World")
+	} else {
+		sc.Scan()
+		a, _ := strconv.Atoi(sc.Text())
+		sc.Scan()
+		b, _ := strconv.Atoi(sc.Text())
+		fmt.Fprintln(writer, a+b)
+	}
+}
