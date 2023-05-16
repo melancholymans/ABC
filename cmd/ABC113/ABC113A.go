ef3936de7aab86bd8ba4e70d4bc782725ab17cda
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-05-17 04:20:33 +0900

    ABC113A.go add

---
+++ cmd/ABC113/ABC113A.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	r := strings.Split(sc.Text(), " ")
+	x, _ := strconv.Atoi(r[0])
+	y, _ := strconv.Atoi(r[1])
+	fmt.Fprintln(writer, x+y/2)
+}
