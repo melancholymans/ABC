7c52440bb0e18c65b22a715a57738a67e7d139dc
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-06-03 10:55:14 +0900

    ABC139B.go add

---
+++ cmd/ABC139/ABC139B.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	r := strings.Split(sc.Text(), " ")
+	a, _ := strconv.Atoi(r[0])
+	b, _ := strconv.Atoi(r[1])
+	fmt.Fprintln(writer, math.Ceil(float64(b-a)/float64(a-1))+1.0)
+}
