8996167f746ab830ca7fdd6f57663cb262ba795e
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-06-10 05:12:14 +0900

    ABC172A.go add

---
+++ cmd/ABC172/ABC172A.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	a, _ := strconv.Atoi(sc.Text())
+	fmt.Fprintln(writer, a*(1+a+a*a))
+}
