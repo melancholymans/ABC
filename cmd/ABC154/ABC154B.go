66c218c068990db77e47d98a60da77e86a28c65c
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-06-10 11:38:38 +0900

    ABC154B.go add

---
+++ cmd/ABC154/ABC154B.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	n := len(sc.Text())
+	fmt.Fprintln(writer, strings.Repeat("x", n))
+}
