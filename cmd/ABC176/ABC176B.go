28a0d4923b69fe42ada1d60cfbfb6178a591cb6c
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-06-18 10:15:37 +0900

    ABC176B.go modified

--- cmd/ABC176/ABC176B.go
+++ cmd/ABC176/ABC176B.go
@@ -3,22 +3,65 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-	sc.Scan()
-	n := sc.Text()
-	total := 0
-	for i := 0; i < len(n); i++ {
-		total += int(n[i] - '0')
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
 	}
-	if total%9 == 0 {
-		fmt.Fprintln(writer, "Yes")
+}
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	defer wtr.Flush()
+	ns := ns()
+	count := 0
+	n := len(ns)
+	for i := 0; i < n; i++ {
+		count += int(ns[i] - '0')
+	}
+	if count%9 == 0 {
+		fmt.Fprintln(wtr, "Yes")
 	} else {
-		fmt.Fprintln(writer, "No")
+		fmt.Fprintln(wtr, "No")
 	}
 }
