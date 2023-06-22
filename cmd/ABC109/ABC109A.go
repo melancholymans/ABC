75807e2eda63295e94eec588048df1bb8724a5e6
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-06-22 19:46:23 +0900

    ABC109A.go add

---
+++ cmd/ABC109/ABC109A.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	s := strings.Split(sc.Text(), " ")
+	a, _ := strconv.Atoi(s[0])
+	b, _ := strconv.Atoi(s[1])
+	if a*b%2 != 0 {
+		fmt.Fprint(writer, "Yes")
+	} else {
+		fmt.Fprint(writer, "No")
+	}
+}
