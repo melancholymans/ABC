3a0b8198a40fd141b3025dd40f46e515f8393ced
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-07-04 05:53:01 +0900

    ABC263A.go add

---
+++ cmd/ABC263/ABC263A.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+func ni5() (int, int, int, int, int) {
+	return ni(), ni(), ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+func main() {
+	defer wtr.Flush()
+	sl := nis(5)
+	mp := map[int]int{}
+	for _, v := range sl {
+		mp[v] += 1
+	}
+	flag1 := false
+	flag2 := false
+	for _, v := range mp {
+		if v == 3 {
+			flag1 = true
+		}
+		if v == 2 {
+			flag2 = true
+		}
+	}
+	if flag1 && flag2 {
+		fmt.Fprintln(wtr, "Yes")
+	} else {
+		fmt.Fprintln(wtr, "No")
+	}
+}
