a68bf167d7806295027cb6fea03a581deab59efe
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-07-15 16:49:31 +0900

    ABC107B.go modififed

--- cmd/ABC107/ABC107B.go
+++ cmd/ABC107/ABC107B.go
@@ -3,107 +3,114 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-	sc.Scan()
-	r1 := strings.Split(sc.Text(), " ")
-	h, _ := strconv.Atoi(r1[0])
-	w, _ := strconv.Atoi(r1[1])
-	sl := make([][]string, h)
-	for i := 0; i < h; {
-		sc.Scan()
-		r2 := strings.Split(sc.Text(), "")
-		if calc(r2) {
-			sl[i] = append(sl[i], r2...)
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
 		}
-		i += 1
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
 	}
-	fmt.Fprintln(writer, sl, "\n", w)
+}
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
 }
 
-func calc(r []string) bool {
+func main() {
+	defer wtr.Flush()
+	h, w := ni2()
+	sl := make([][]string, h)
+	for i := 0; i < h; i++ {
+		sl[i] = make([]string, w)
+		s := strings.Split(ns(), "")
+		for j := 0; j < w; j++ {
+			sl[i][j] = s[j]
+		}
+	}
+	cp1 := make([][]string, 0)
+	for i := 0; i < h; i++ {
+		if calc(sl[i]) {
+			cp1 = append(cp1, sl[i])
+		}
+	}
+	r := rotate(cp1)
+	cp2 := make([][]string, 0)
 	for i := 0; i < len(r); i++ {
-		if r[i] == "#" {
+		if calc(r[i]) {
+			cp2 = append(cp2, r[i])
+		}
+	}
+	r = rotate(cp2)
+	for i := 0; i < len(r); i++ {
+		for j := 0; j < len(r[0]); j++ {
+			fmt.Fprint(wtr, r[i][j])
+		}
+		fmt.Fprintln(wtr, " ")
+	}
+}
+
+func calc(s []string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] == "#" {
 			return true
 		}
 	}
 	return false
 }
 
-/*
-func Sli(l, m int, def int) [][]int {
-	sl := make([][]int, l)
-	for i := 0; i < l; i++ {
-		sl[i] = make([]int, m)
-		for j := 0; j < m; j++ {
-			sl[i][j] = def
-		}
-	}
-	return sl
-}
-*/
-/*
-package main
-
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-	//sc.Scan()
-	//n, _ := strconv.Atoi(sc.Text())
-	h := 2
-	w := 4
-	sl := make([][]string, h)
-	for i := 0; i < h; i++ {
-		sl[i] = make([]string, w)
-		sc.Scan()
-		r := strings.Split(sc.Text(), " ")
-		for j := 0; j < w; j++ {
-			sl[i][j] = r[j]
-		}
-	}
-	ad := turnRight(h, w, sl)
+func rotate(sl [][]string) [][]string {
+	h, w := len(sl), len(sl[0])
+	r := make([][]string, w)
 	for i := 0; i < w; i++ {
+		r[i] = make([]string, h)
 		for j := 0; j < h; j++ {
-			fmt.Fprint(writer, ad[i][j], " ")
-		}
-		fmt.Fprint(writer, "\n")
-	}
-}
-
-func turnRight(h, w int, sl [][]string) [][]string {
-	ad := make([][]string, w)
-	for i := 0; i < w; i++ {
-		ad[i] = make([]string, h)
-		for j := 0; j < h; j++ {
-			ad[i][j] = sl[h-1-j][i]
+			r[i][j] = sl[j][i]
 		}
 	}
-	return ad
+	return r
 }
-
-func turnLeft(h, w int, sl [][]string) [][]string {
-	ad := make([][]string, w)
-	for i := 0; i < w; i++ {
-		ad[i] = make([]string, h)
-		for j := 0; j < h; j++ {
-			ad[i][j] = sl[j][w-1-i]
-		}
-	}
-	return ad
-}
-*/
