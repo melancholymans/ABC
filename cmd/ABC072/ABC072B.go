c6ee27d32cdf332a2c227258782f8e27b5e5049e
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-07-16 16:38:03 +0900

    ABC072B.go a-modified

--- cmd/ABC072/ABC072B.go
+++ cmd/ABC072/ABC072B.go
@@ -3,21 +3,72 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+func ni() int {
 	sc.Scan()
-	s := sc.Text()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func main() {
+	defer wtr.Flush()
+	s := ns()
 	sl := make([]string, 0)
 	for i := 0; i < len(s); i++ {
-		if i%2 == 0 {
+		if (i+1)%2 != 0 {
 			sl = append(sl, string(s[i]))
 		}
 	}
-	fmt.Fprintln(writer, strings.Join(sl, ""))
+	fmt.Fprintln(wtr, strings.Join(sl, ""))
 }
