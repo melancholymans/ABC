71af7a6de1e91116c798b12b309394696a5283f3
joshua <mattbew64@gmail.com>
2023-08-05 07:52:56 +0900

    ABC297B.go modified

--- cmd/ABC287/ABC287B.go
+++ cmd/ABC287/ABC287B.go
@@ -63,35 +63,22 @@ func nf() float64 {
 
 func main() {
 	defer wtr.Flush()
-	s := ns()
-	ki := 0
-	r1i, r2i := 0, 0
-	b1i, b2i := 0, 0
-	for i := 0; i < 8; i++ {
-		switch string(s[i]) {
-		case "K":
-			ki = i + 1
-		case "R":
-			if r1i == 0 {
-				r1i = i + 1
-			} else {
-				r2i = i + 1
-			}
-		case "B":
-			if b1i == 0 {
-				b1i = i + 1
-			} else {
-				b2i = i + 1
+	n, m := ni2()
+	sl := make([]string, n)
+	for i := 0; i < n; i++ {
+		sl[i] = ns()[3:]
+	}
+	mt := map[string]bool{}
+	for i := 0; i < m; i++ {
+		mt[ns()] = true
+	}
+	count := 0
+	for i := 0; i < n; i++ {
+		for k, _ := range mt {
+			if sl[i] == k {
+				count += 1
 			}
 		}
 	}
-	if ki < r1i || ki > r2i {
-		fmt.Fprintln(wtr, "No")
-		return
-	}
-	if b1i%2 == b2i%2 {
-		fmt.Fprintln(wtr, "No")
-		return
-	}
-	fmt.Fprintln(wtr, "Yes")
+	fmt.Fprintln(wtr, count)
 }
