bcdd6378b7e48bd23e5af14125f69243f513eb37
joshua <mattbew64@gmail.com>
2023-08-06 06:20:49 +0900

    ABC299B.go add

--- cmd/ABC299/ABC299B.go
+++ cmd/ABC299/ABC299B.go
@@ -3,33 +3,87 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-	sc.Scan()
-	r1 := strings.Split(sc.Text(), " ")
-	n, _ := strconv.Atoi(r1[0])
-	t, _ := strconv.Atoi(r1[1])
-	sc.Scan()
-	r2 := strings.Split(sc.Text(), " ")
-	cd := make([]int, 0)
-	for i := 0; i < n; i++ {
-		c, _ := strconv.Atoi(r1[i])
-		cd = append(cd, c)
-	}
-	rd := make([]int, 0)
-	for i := 0; i < n; i++ {
-		r, _ := strconv.Atoi(r2[i])
-		cd = append(rd, r)
-	}
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
 
-	fmt.Fprintln(writer, n, t)
-	fmt.Fprintln(writer, cd)
-	fmt.Fprintln(writer, rd)
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func main() {
+	defer wtr.Flush()
+	n, t := ni2()
+	c := nis(n)
+	r := nis(n)
+	mmax := 0
+	player := 0
+	color := 0
+	for i := 0; i < n; i++ {
+		if c[i] == t {
+			color = t
+		}
+	}
+	if color == 0 {
+		color = c[0]
+	}
+	for i := 0; i < n; i++ {
+		if c[i] == color {
+			if mmax < r[i] {
+				mmax = r[i]
+				player = i + 1
+			}
+		}
+	}
+	fmt.Fprintln(wtr, player)
 }
