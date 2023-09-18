5944828a731f6a92db6e13911624d7ed96e5a139
joshua <mattbew64@gmail.com>
2023-09-18 16:02:50 +0900

    ABC060B.go modified

--- cmd/ABC060/ABC060B.go
+++ cmd/ABC060/ABC060B.go
@@ -17,5 +17,24 @@ func main() {
 	a, _ := strconv.Atoi(r[0])
 	b, _ := strconv.Atoi(r[1])
 	c, _ := strconv.Atoi(r[2])
-	fmt.Fprintln(writer, a, b, c)
+	d := gcd(a, b)
+	if c%d == 0 {
+		fmt.Fprintln(writer, "YES")
+	} else {
+		fmt.Fprintln(writer, "NO")
+	}
+}
+
+func gcd(a, b int) int {
+	for a >= 1 && b >= 1 {
+		if a < b {
+			b = b % a
+		} else {
+			a = a % b
+		}
+	}
+	if a >= 1 {
+		return a
+	}
+	return b
 }
