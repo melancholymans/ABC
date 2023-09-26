01faaa3aaa43c4c9c444ac115e42aade853708cf
joshua <mattbew64@gmail.com>
2023-09-27 05:27:17 +0900

    ABC100B.go modified

--- cmd/ABC100/ABC100B.go
+++ cmd/ABC100/ABC100B.go
@@ -3,24 +3,77 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-	sc.Scan()
-	r := strings.Split(sc.Text(), " ")
-	d, _ := strconv.Atoi(r[0])
-	n, _ := strconv.Atoi(r[1])
-	if d == 0 {
-		fmt.Fprintln(writer, n+n/100)
-	} else if d == 1 {
-		fmt.Fprintln(writer, 100*n)
-	} else {
-		fmt.Fprintln(writer, 10000*n)
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
 	}
+}
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func main() {
+	defer wtr.Flush()
+	d, n := ni2()
+	if n == 100 {
+		n += 1
+	}
+	rst := pow10(d) * pow10(d) * n
+	fmt.Fprintln(wtr, rst)
+}
+
+var pow10cache [20]int
+
+func pow10(i int) int {
+	if pow10cache[i] == 0 {
+		pow10cache[i] = int(math.Pow(10, float64(i)))
+	}
+	return pow10cache[i]
 }
