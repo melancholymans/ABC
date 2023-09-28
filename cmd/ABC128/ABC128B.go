d7d074fec836e9bedfa7a3b6800a5aef37617a7c
joshua <mattbew64@gmail.com>
2023-09-29 06:52:31 +0900

    ABC128B.go modified

--- cmd/ABC128/ABC128B.go
+++ cmd/ABC128/ABC128B.go
@@ -3,22 +3,85 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-	sc.Scan()
-	n, _ := strconv.Atoi(sc.Text())
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
 
-	for i := 0; i < n; i++ {
-		sc.Scan()
-		r1 := strings.Split(sc.Text(), " ")
-		fmt.Fprintln(writer, r1)
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
 	}
-	fmt.Fprintln(writer, n)
+}
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+type restaurant struct {
+	no    int
+	score int
+	name  string
+}
+
+func main() {
+	defer wtr.Flush()
+	n := ni()
+	sl := make([]restaurant, n)
+	for i := 0; i < n; i++ {
+		sl[i].no = i + 1
+		sl[i].name = ns()
+		sl[i].score = ni()
+	}
+	sort.Slice(sl, func(i, j int) bool {
+		if sl[i].name == sl[j].name {
+			return sl[i].score > sl[j].score
+		}
+		return sl[i].name < sl[j].name
+	})
+	for i := 0; i < n; i++ {
+		fmt.Fprintln(wtr, sl[i].no)
+	}
 }
