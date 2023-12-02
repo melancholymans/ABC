ebd66aa9e1c5b3a6a477faf5b3a4f490829810b9
joshua <mattbew64@gmail.com>
2023-12-02 11:18:08 +0900

    ABC016C.go add

--- cmd/ABC016/ABC016C.go
+++ cmd/ABC016/ABC016C.go
@@ -58,18 +58,109 @@ func nf() float64 {
 	return f
 }
 
+type compFunc func(p, q interface{}) int
+
+type state struct {
+	score int
+	node  int
+}
+
+type edge struct {
+	from int
+	to   int
+	cost int
+	rev  int
+}
+
+type graph struct {
+	size         int
+	edges        [][]edge
+	starts       []state
+	comps        []compFunc
+	defaultScore int
+	level        []int
+	iter         []int
+}
+
 func main() {
 	defer wtr.Flush()
 	n, m := ni2()
-	sl := make([][]bool, n)
-	for i := 0; i < n; i++ {
-		sl[i] = make([]bool, n)
-	}
+	edges := make([][]edge, n)
 	for i := 0; i < m; i++ {
-		a := ni() - 1
-		b := ni() - 1
-		sl[a][b] = true
-		sl[b][a] = true
+		s := ni() - 1
+		t := ni() - 1
+		c := 1
+		edges[s] = append(edges[s], edge{to: t, cost: c})
+		edges[t] = append(edges[t], edge{to: s, cost: c})
 	}
-	fmt.Fprintln(wtr, sl)
+	g := newGraph(n, edges)
+	dist, _ := g.floydWarshall()
+	for i := 0; i < n; i++ {
+		t := 0
+		for j := 0; j < n; j++ {
+			if dist[i][j] == 2 {
+				t += 1
+			}
+		}
+		fmt.Fprintln(wtr, t)
+	}
+}
+
+func newGraph(size int, edges [][]edge) *graph {
+	graph := &graph{
+		size:  size,
+		edges: edges,
+	}
+	graph.defaultScore = math.MaxInt64
+	graph.comps = []compFunc{
+		func(p, q interface{}) int {
+			if p.(state).score < q.(state).score {
+				return -1
+			} else if p.(state).score == q.(state).score {
+				return 0
+			}
+			return 1
+		},
+	}
+	return graph
+}
+
+func (g *graph) floydWarshall() ([][]int, bool) {
+	score := make([][]int, g.size)
+	for i := 0; i < g.size; i++ {
+		score[i] = make([]int, g.size)
+		for j := 0; j < g.size; j++ {
+			if i == j {
+				score[i][j] = 0
+			} else {
+				score[i][j] = g.defaultScore
+			}
+		}
+		for _, edge := range g.edges[i] {
+			score[i][edge.to] = edge.cost
+		}
+	}
+	for k := 0; k < g.size; k++ {
+		for i := 0; i < g.size; i++ {
+			for j := 0; j < g.size; j++ {
+				if score[i][k] == g.defaultScore || score[k][j] == g.defaultScore {
+					continue
+				}
+				score[i][j] = min(score[i][j], score[i][k]+score[k][j])
+			}
+		}
+	}
+	for k := 0; k < g.size; k++ {
+		if score[k][k] < 0 {
+			return nil, true
+		}
+	}
+	return score, false
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
