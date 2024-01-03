7a0a899137a05277d63bf34f91e7e03c905c9f6f
joshua <mattbew64@gmail.com>
2024-01-03 10:54:21 +0900

    ABC030C.go add

---
+++ cmd/ABC030/ABC030C.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+func ni2() (int, int) {
+	return ni(), ni()
+}
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func main() {
+	defer wtr.Flush()
+	count := 0
+	n, m := ni2()
+	a, b := ni2()
+	ns := nis(n)
+	ms := nis(m)
+	//tは現在時間
+	t := 0
+	for {
+		//count==0のときcount%2=>0になるので、このif文内はA空港からの出発を処理している
+		if count%2 == 0 {
+			ti := upperBound(t-1, ns)
+			if ti == n {
+				break
+			}
+			t = ns[ti] + a
+		} else {
+			ti := upperBound(t-1, ms)
+			if ti == m {
+				break
+			}
+			t = ms[ti] + b
+		}
+		count += 1
+	}
+	count /= 2
+	fmt.Fprintln(wtr, count)
+}
+
+func upperBound(v int, sl []int) int {
+	if len(sl) == 0 {
+		return 0
+	}
+	idx := bs(0, len(sl)-1, func(c int) bool {
+		return sl[c] <= v
+	})
+	return idx + 1
+}
+
+func bs(ok, ng int, f func(int) bool) int {
+	if !f(ok) {
+		return -1
+	}
+	if f(ng) {
+		return ng
+	}
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+		if f(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+	return ok
+}
+
+func abs(a int) int {
+	if 0 > a {
+		return -a
+	}
+	return a
+}
