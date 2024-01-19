ca8b1ad2305b499dac91a26c536058129344fa01
joshua <mattbew64@gmail.com>
2024-01-19 17:56:37 +0900

    ABC209C.go add

---
+++ cmd/ABC209/ABC209C.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := os.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	defer wtr.Flush()
+	n := ni()
+	sl := nis(n)
+	total := 1
+	sort.Slice(sl, func(i, j int) bool {
+		return sl[i] < sl[j]
+	})
+	for i := 0; i < n; i++ {
+		t := sl[i] - i
+		if t <= 0 {
+			fmt.Fprintln(wtr, 0)
+			return
+		}
+		total = mmul(total, t)
+	}
+	fmt.Fprintln(wtr, total)
+}
+
+func mmul(a, b int) int {
+	return a * b % 1000000007
+}
