3ea3f51243251786f8e5e4e8a2dbea6d7c21f521
joshua <mattbew64@gmail.com>
2024-03-17 06:27:50 +0900

    ABC345A.go add

---
+++ cmd/ABC345/ABC345A.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := os.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	defer wtr.Flush()
+	s := ns()
+	f := strings.Index(s, "<")
+	fc := strings.Count(s, "<")
+	if f == -1 {
+		fmt.Fprintln(wtr, "No")
+		return
+	}
+	if fc != 1 {
+		fmt.Fprintln(wtr, "No")
+		return
+	}
+	m := strings.Index(s, "=")
+	if m == -1 {
+		fmt.Fprintln(wtr, "No")
+		return
+	}
+	l := strings.Index(s, ">")
+	lc := strings.Count(s, ">")
+	if l == -1 {
+		fmt.Fprintln(wtr, "No")
+		return
+	}
+	if lc != 1 {
+		fmt.Fprintln(wtr, "No")
+		return
+	}
+	if f < m && m < l {
+		fmt.Fprintln(wtr, "Yes")
+		return
+	}
+	fmt.Fprintln(wtr, "No")
+}
