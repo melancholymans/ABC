d52be476efab9c08056229a80936b375e1a47099
joshua <mattbew64@gmail.com>
2024-05-05 16:44:50 +0900

    ABC244C.go commit

---
+++ cmd/ABC244/ABC244C.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := os.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	defer wtr.Flush()
+	var n int
+	fmt.Scanf("%d", &n)
+	l := n*2 + 1
+	sl := make([]int, l)
+	for {
+		for i := 0; i < l; i++ {
+			if sl[i] == 0 {
+				sl[i] = 1
+				fmt.Printf("%d\n", i+1)
+				break
+			}
+		}
+		var in int
+		fmt.Scanf("%d", &in)
+		if in == 0 {
+			break
+		}
+		sl[in-1] = 1
+	}
+}
