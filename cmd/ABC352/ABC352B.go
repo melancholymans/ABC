76be63244325a49da1e73fcf3495836e8a251713
joshua <mattbew64@gmail.com>
2024-05-05 11:43:51 +0900

    ABC352B.go commit

---
+++ cmd/ABC352/ABC352B.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := os.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nis(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return a
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	defer wtr.Flush()
+	s := ns()
+	t := ns()
+	for i := 0; ; {
+		for j := 0; ; {
+			if s[i] == t[j] {
+				fmt.Fprint(wtr, j+1, " ")
+				i += 1
+			}
+			j += 1
+			if i == len(s) {
+				fmt.Fprintln(wtr, " ")
+				return
+			}
+		}
+	}
+	fmt.Fprintln(wtr, " ")
+}
